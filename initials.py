5dab0676e2aaabba5979cb003d1821df38827cbd
Elizabeth-36 <2425403844@live.gctu.edu.gh>
2026-05-31 17:34:26 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,39 @@
+# File: initials.py
+# Description: Print out of my initials (MAE)
+# Assignment Number: 1
+#
+# Name: AYERTEY ELIZABETH
+# STUDENT ID: 2425403844  
+# Email:2425403844@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+# On my honor, AYERTEY ELIZABETH, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Prints initials M, A, E in large 12-wide x 10-tall block letters where M stands for Ms
+    #  each letter is followed by a 2x2 asterisk
+    #  Three dot columns appear left and right of each letter and group of periods
+    #  No trailing spaces on any line.
+    # No loops, conditionals, or f-strings are used only print() and strings.
+
+    print()
+    print("...MAE")
+    print()
+
+    print("...MMMM...MMMM.............AAAA............EEEEEEEEEEEE")
+    print("...MMMM...MMMM............AA..AA...........EEEEEEEEEEEE")
+    print("...MM.MM..MM.MM..........AA....AA..........EE")
+    print("...MM.MM..MM.MM.........AA......AA.........EE")
+    print("...MM..MM.MM.MM........AA........AA........EEEEEEEEEE")
+    print("...MM..MMMM..MM........AAAAAAAAAAAA........EEEEEEEEEE")
+    print("...MM...MM...MM........AA........AA........EE")
+    print("...MM...MM...MM........AA........AA........EE")
+    print("...MM...MM...MM...**...AA........AA...**...EEEEEEEEEEEE**")
+    print("...MM...MM...MM...**...AA........AA...**...EEEEEEEEEEEE**")
+
+    print()
 
 
-
-main()
+if __name__ == "__main__":
+    main()
